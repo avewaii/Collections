384b9459354031a1bea0cb8b0f36b6bf58b78697
avewaii <ewashadevsk@gmail.com>
2021-02-03 00:15:09 +0300

    Heroku

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let homeRouter = require('./routes/home');
 let sessionsRouter = require('./routes/sessions');
@@ -12,21 +11,16 @@ let collectionsRouter = require('./routes/collections');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/', sessionsRouter);
-app.use('/', collectionsRouter);
-app.use('/', homeRouter)
+app.use('/api/users', usersRouter);
+app.use('/api/', sessionsRouter);
+app.use('/api/', collectionsRouter);
+app.use('/api/', homeRouter)
 
 
 // catch 404 and forward to error handler
